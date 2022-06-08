8cd55901ce5f0ac35a44dcca68d4f3843a53456a
Starkiller28 <sebastob2000@gmail.com>
2022-06-08 13:28:54 -0500

    Changing the content of round.py

--- classes/round.py
+++ classes/round.py
@@ -1,22 +1,21 @@
 import random
+from classes.question import Question
 
 
 class Round:
     def __init__(self) -> None:
         self.__score = 0
-        self.__name = ""
+        self.__player = ""
         self.__n_round = 0
         self.__answers = []
         self.__question = ""
 
     # Getters
-    
 
     # Properties
-    
 
     # Private methods
-    
+    def __upload_player(self) -> None:
+        pass
 
     # Public methods
-    
